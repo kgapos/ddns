9b623db60f8e11ace0351cd443035bf68e31cecf
Kostas Apostolopoulos <kgapos@outlook.com>
2023-04-07 04:33:22 +0100

    Implement basic functionality (#1)
    * Add CONTRIBUTING.md file
    * Implement basic functionality

---
+++ README.md
@@ -0,0 +1,24 @@
+# Description
+This script automatically updates the A records for a list of FQDNs in GoDaddy with the current public IP address of the machine running the script. It checks if the public IP has changed; if so, it uses the GoDaddy API to update the A records for the specified subdomains. It also sends an email notification using an IFTTT webhook. The script is designed to be used in situations where a home server or device has a dynamic public IP that may change periodically, and the DNS records need to be updated accordingly to maintain accessibility.
+
+# Setup
+Grant execute permissions with `sudo chmod +x run_ddns.sh`.
+
+Copy the `config.sh.example` into a new `config.sh` file and configure the following:
+1. FQDNS: The list of DNS records to be monitored and updated to the servers public IP
+2. GODADDY_SSO: Your GoDaddy "key:secret" from https://developer.godaddy.com/keys
+3. IFTTT_KEY: Your IFTTT webhook key from https://ifttt.com/maker_webhooks
+4. IFTTT_EVENT: Your IFTTT event name, as specified when creating the webhook
+
+> Note: For more information check [the IFTTT webhooks documentation](https://ifttt.com/maker_webhooks). The IFTTT webhooks API endpoint looks like this `https://maker.ifttt.com/trigger/{IFTTT_EVENT}/json/with/key/{IFTTT_KEY}`
+
+# Use
+* Run on-demand with `./run_ddns.sh`
+* Check the logs with `tail -f log.csv`
+* Schedule as a cronjob by adding a crontab command with `crontab -e`
+
+Example crontab command that runs every 5 minutes:
+```
+# m h dom mon dow command
+*/5 * * * * /path/to/godaddy_ddns/run_ddns.sh
+```
---
+++ run_ddns.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Stop the script if any command returns a non-zero exit code 
+set -e
+
+# Configuration file
+source config.sh
+
+# Function to log messages to the log file with timestamps
+log_message() {
+  TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+  EVENT="$1"
+  MESSAGE="$2"
+  echo "$TIMESTAMP,$EVENT,$MESSAGE" >> "$LOG_FILE"
+  echo "$TIMESTAMP,$EVENT,$MESSAGE"
+}
+
+log_message "STARTED" "GoDaddy DDNS execution started"
+SERVER_IP=$(curl -s https://api.ipify.org)
+log_message "CHECKED_SERVER_IP" "Server's current public IP: $SERVER_IP"
+
+for FQDN in "${FQDNS[@]}"; do
+  DOMAIN="$(echo "$FQDN" | rev | cut -d. -f1-2 | rev)"
+  SUBDOMAIN="$(echo "$FQDN" | cut -d. -f1)"
+  DOMAIN_IP=$(curl -s -X GET -H "Authorization: sso-key $GODADDY_SSO" \
+    "https://api.godaddy.com/v1/domains/$DOMAIN/records/A/$SUBDOMAIN" | jq -r '.[].data')
+  log_message "CHECKED_DOMAIN_IP" "Current GoDaddy IP for $FQDN: $DOMAIN_IP"
+
+  if [ "$SERVER_IP" != "$DOMAIN_IP" ]; then
+    # Send email notification through IFTTT webhook
+    curl -s -X POST \
+      -H "Content-Type: application/json" \
+      -d "{\"message\": \"Updating $FQDN from $DOMAIN_IP to $SERVER_IP...\"}" \
+      "https://maker.ifttt.com/trigger/$IFTTT_EVENT/json/with/key/$IFTTT_KEY" > /dev/null
+    log_message "INVOKED_IFTTT_WEBHOOK" "Invoked IFTTT webhook to send email notification"
+    # Update the A record
+    curl -s -X PUT \
+      -H "Authorization: sso-key $GODADDY_SSO" \
+      -H "Content-Type: application/json" \
+      -d "[{\"data\": \"$SERVER_IP\", \"ttl\": 600}]" \
+      "https://api.godaddy.com/v1/domains/$DOMAIN/records/A/$SUBDOMAIN"
+    log_message "UPDATED_GODADDY" "Updated $FQDN from $DOMAIN_IP to $SERVER_IP..."
+  fi
+done
+log_message "COMPLETED" "GoDaddy DDNS execution complete"
